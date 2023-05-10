af2ed5a5fdf51d0ed091370514cfa456922d7fd9
umathileepan <thileepan_uma@yahoo.co.uk>
2023-05-10 20:47:23 +0100

    Primitive Types, Strinng, and Control flow

--- LearningJava/src/Main.java
+++ LearningJava/src/Main.java
@@ -1,19 +1,46 @@
 import java.sql.SQLOutput;
 
+import static java.lang.System.in;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World from the IntelliJ IDE!");
 
         int studentAge = 15;
         double studentGPA = 3.45;
-        char studentFirstInitial = 'K';
-        char studentLastInitial = 'H';
+
         boolean hasPerfectAttendance = true;
+        String studentFirstName = "Jones";
+        String studentLastName = "Adam";
+
+        char studentFirstInitial = studentFirstName.charAt(0);
+        char studentLastInitial = studentLastName.charAt(0);
 
         System.out.println(studentAge);
         System.out.println(studentGPA);
         System.out.println(studentFirstInitial);
         System.out.println(studentLastInitial);
         System.out.println(hasPerfectAttendance);
+        System.out.println(studentFirstName);
+        System.out.println(studentLastName);
+        System.out.println(studentFirstName + " " + studentLastName + " has a GPA " + studentGPA);
+
+        System.out.println("What do you want to update it to?");
+        Scanner input = new Scanner(in);
+        studentGPA = input.nextDouble();
+        System.out.println(studentFirstName + " " + studentLastName + " has a GPA " + studentGPA);
+
+        System.out.println("Pick a number between 1 - 10");
+        Scanner num = new Scanner(System.in);
+        int pickedNum = num.nextInt();
+
+        if(pickedNum < 5){
+            System.out.println("Enjoy the good luck friends bring you");
+        } else{
+            System.out.println("You shoes bring your luck" +
+                    "");
+        }
+
     }
 }
