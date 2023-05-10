a368b7611bf91c2cfc436b6906e5aa6b5de301ca
umathileepan <thileepan_uma@yahoo.co.uk>
2023-05-10 20:36:37 +0100

    Primitive Type

---
+++ LearningJava/src/Main.java
@@ -0,0 +1,19 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World from the IntelliJ IDE!");
+
+        int studentAge = 15;
+        double studentGPA = 3.45;
+        char studentFirstInitial = 'K';
+        char studentLastInitial = 'H';
+        boolean hasPerfectAttendance = true;
+
+        System.out.println(studentAge);
+        System.out.println(studentGPA);
+        System.out.println(studentFirstInitial);
+        System.out.println(studentLastInitial);
+        System.out.println(hasPerfectAttendance);
+    }
+}
