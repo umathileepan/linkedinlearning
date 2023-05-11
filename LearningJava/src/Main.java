909fe5349418076d7a948bc194b762374a80a689
umathileepan <thileepan_uma@yahoo.co.uk>
2023-05-11 12:00:29 +0100

    Challenge of Multiple choice coding

--- LearningJava/src/Main.java
+++ LearningJava/src/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World from the IntelliJ IDE!");
+        /*System.out.println("Hello World from the IntelliJ IDE!");
 
         int studentAge = 15;
         double studentGPA = 3.45;
@@ -27,7 +27,7 @@ public class Main {
         System.out.println(studentFirstName + " " + studentLastName + " has a GPA " + studentGPA);
 
         System.out.println("What do you want to update it to?");
-        Scanner input = new Scanner(in);
+        Scanner input = new Scanner(System.in);
         studentGPA = input.nextDouble();
         System.out.println(studentFirstName + " " + studentLastName + " has a GPA " + studentGPA);
 
@@ -38,9 +38,46 @@ public class Main {
         if(pickedNum < 5){
             System.out.println("Enjoy the good luck friends bring you");
         } else{
-            System.out.println("You shoes bring your luck" +
-                    "");
+            System.out.println("You shoes bring your luck");
         }
+
+        Scanner scanner = new Scanner(System.in);
+        boolean onSongRepeat = true;
+        while(onSongRepeat) {
+            System.out.println("Current song is playing");
+            System.out.println("Would you like not to repeat the song? If so answer : yes");
+            String answer = scanner.next();
+
+            if (answer.equals("yes")) {
+                onSongRepeat = false;
+            }
+        }
+            System.out.println("Next song is playing");*/
+
+        String question = "What is the java code to print something in the screen?";
+        String choiceOne = "system.in";
+        String choiceTwo = "System.out.println()";
+        String choiceThree = "system.out";
+
+        String correctAnswer = choiceTwo;
+
+        System.out.println(question);
+        System.out.println(choiceOne);
+        System.out.println(choiceTwo);
+        System.out.println(choiceThree);
+
+        Scanner scanner = new Scanner(System.in);
+        String answer = scanner.next();
+
+        if(correctAnswer.equals(answer)){
+            System.out.println("Congratulation! Your answer is correct.");
+        }else{
+            System.out.println("Good luck next time! Your answer is wrong.");
+        }
+
+
+
+
 
     }
 }
