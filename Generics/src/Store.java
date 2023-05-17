740235fd58c5b655dfa65f9e016159146908b1f1
umathileepan <thileepan_uma@yahoo.co.uk>
2023-05-17 09:49:39 +0100

    Queue interface has been implemented by Linked List to serve customer.

---
+++ Generics/src/Store.java
@@ -0,0 +1,20 @@
+import java.util.LinkedList;
+
+public class Store {
+    //Using queue interface to serve customer
+    public static void main(String[] args){
+        LinkedList<Customer> queue = new LinkedList<>();
+        queue.add(new Customer("Sally"));
+        queue.add(new Customer("Ben"));
+        queue.add(new Customer("Emma"));
+        queue.add(new Customer("Fred"));
+        System.out.println(queue);
+        serveCustomer(queue);
+        System.out.println(queue);
+    }
+
+    static void serveCustomer(LinkedList<Customer> queue){
+        Customer c = queue.poll();
+        c.serve();
+    }
+}
