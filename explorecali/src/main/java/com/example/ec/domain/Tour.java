110acfda6ae9ecedeefca95c55a39784c81cabea
umathileepan <thileepan_uma@yahoo.co.uk>
2023-05-24 21:31:15 +0100

    Working with Spring Boot.

---
+++ explorecali/src/main/java/com/example/ec/domain/Tour.java
@@ -0,0 +1,52 @@
+@Entity
+public class Tour{
+    @Id
+    @GeneratedValu
+    private Integer id;
+
+    @Column
+    private String title;
+
+    @Column(length = 2000)
+    private String description;
+
+    @Column(length = 2000)
+    private String blurb;
+
+    @Column
+    private Interger price;
+
+    @Column
+    private String duration;
+
+    @Column(length = 2000)
+    private String bullets;
+
+    @Column
+    private String keywords;
+
+    @ManyToOne
+    private TourPackage tourPackage;
+
+    @Column
+    @Enumerated
+    private Difficulty difficulty;
+
+    @Column
+    @Enumerated
+    private Region region;
+
+    public Tour(Integer id, String title, String description, String blurb, Interger price, String duration, String bullets, String keywords,TourPackage tourPackage,Difficulty difficulty,Region region) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.blurb = blurb;
+        this.price = price;
+        this.duration = duration;
+        this.bullets = bullets;
+        this.keywords = keywords;
+        this.tourPackage = tourPackage;
+        this.difficulty = difficulty;
+        this.region = region;
+    }
+}
