d55a87f1489f50f805bb75a49451962b07463573
umathileepan <thileepan_uma@yahoo.co.uk>
2023-06-04 18:19:54 +0100

    use of template literal.

---
+++ JavaScript/Backpack.js
@@ -0,0 +1,36 @@
+class Backpack{
+    constructor(
+        name,
+        volume,
+        color,
+        pocketNum,
+        strapLengthL,
+        strapLengthR,
+        lidOpen
+    )
+    {
+        this.name=name;
+        this.volume=volume;
+        this.color=color;
+        this.pocketNum=pocketNum;
+        this.strapLength={
+            left: strapLengthL,
+            right: strapLengthR
+        };
+        this.lidOpen=lidOpen;
+    }
+
+    //Add method like normal function
+    toggleLid(lidStatus){
+        this.lidOpen = lidStatus;
+    }
+
+    newStrapLength(lengthLeft, lengthRight)
+    {
+        this.strapLength.left=lengthLeft;
+        this.strapLength.right=lengthRight;
+    }
+}
+
+
+export default Backpack;
--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,4 +1,5 @@
-import Book from "./Book.js";
+import Book from "../Book.js";
+import Backpack from "./Backpack.js";
 
 const book = new Book(
     "Healthy Life Style",
@@ -11,3 +12,34 @@ const book = new Book(
 console.log("book details: ",book);
 console.log("Colour of the : ", book.changeBookCover("green","Tree"));
 console.log("after the change of book cover: ", book);
+
+const everydayPack = new Backpack(
+    "Everyday Backpack",
+    30,
+    "grey",
+    15,
+    26,
+    26,
+    false
+);
+
+console.log("The everydayPack object: ",everydayPack);
+
+const content = `
+<main>
+    <article>
+        <h1> Everyday Backpack</h1>
+        <ul>
+            <li>Volume: ${everydayPack.volume} </li>
+            <li>Color: ${everydayPack.Color} </li>
+            <li>Number of pockets: ${everydayPack.pocketNum}</li>
+            <li>Left strap length: ${everydayPack.strapLength.left}</li>
+            <li>Right strap length: ${everydayPack.strapLength.right} </li>
+            <li>Lid Status: ${everydayPack.lidOpen}</li>
+        </ul>
+    </article>
+</main>
+`
+
+document.body.innerHTML = content;
+document.querySelectorAll("main li").forEach(item => item.style.backgroundColor="blue");
