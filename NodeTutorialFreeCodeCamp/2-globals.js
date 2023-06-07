cf75d229102221fd360fb40c585f41c815bccd0f
umathileepan <thileepan_uma@yahoo.co.uk>
2023-06-07 21:35:12 +0100

    Commit all the pending

--- NodeTutorialFreeCodeCamp/2-globals.js
+++ NodeTutorialFreeCodeCamp/2-globals.js
@@ -3,3 +3,34 @@ console.log(__dirname);
 setInterval(()=>{
     console.log('Well done!!!')
 }, 1000)
+
+
+setInterval(compName = (name)=>{
+    if(name==='Uma'){
+        console.log('You are back on track');
+    }else{
+        console.log('I don\'t know you');
+    }
+},5000)
+
+compName('Uma');
+
+const path = require("path");
+console.log(`The file name is ${path.basename(__filename)}`);
+
+for(let key in global){
+    console.log(key);
+}
+
+console.log(process.argv);
+
+function grab(flag){
+    let indexAfterFlag = process.argv.indexOf(flag) + 1;
+    return process.argv[indexAfterFlag];
+}
+
+let greeting = grab("--greeting");
+let user = grab("--user");
+
+console.log(greeting);
+console.log(user);
---
+++ NodeTutorialFreeCodeCamp/3-Modules.js
@@ -0,0 +1,10 @@
+//Modules
+
+
+const name1 = 'Uma';
+
+const sayHi = ((name) =>{
+    console.log(`Hello  ${name}`);
+});
+
+sayHi(name1);
