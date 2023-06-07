e47633b91aafd23e5d3bc1eeb2f81b661793f61b
umathileepan <thileepan_uma@yahoo.co.uk>
2023-06-07 22:14:41 +0100

    Standard input and output

---
+++ NodeTutorialFreeCodeCamp/app.js
@@ -0,0 +1,34 @@
+//Working with standard input
+
+process.stdout.write("Hello \n \n");
+
+const questions = [
+    "What is your name?",
+    "What would you rather be doing?",
+    "What is your preferred programming language?"
+];
+
+const answers = [];
+
+function ask(i = 0){
+    process.stdout.write(`\n\n\n ${questions[i]}`);
+    process.stdout.write(` > `);
+}
+
+ask();
+
+process.stdin.on("data", function (data){
+    answers.push(data.toString().trim());
+    if(answers.length < questions.length){
+        ask(answers.length);
+    }else{
+        process.exit();
+    }
+});
+
+process.on("exit", function(){
+    process.stdout.write("\n\n\n\n");
+    process.stdout.write(
+        `Go ${answers[1]} ${answers[0]} you can finish programming ${answers[2]} later`
+    );
+});
