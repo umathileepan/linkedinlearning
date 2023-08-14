8003140439f2769d9f641689a4c90d8962eb5292
umathileepan <thileepan_uma@yahoo.co.uk>
2023-08-14 08:00:08 +0100

    Console level programing.

---
+++ C#.NET/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string response;
+Console.WriteLine("What is you name? ");
+response = Console.ReadLine();
+Console.WriteLine($"Enjoy your course, {response}");
+OperatingSystem thisOS = Environment.OSVersion;
+Console.WriteLine(thisOS.Platform);
+Console.WriteLine(thisOS.VersionString);
