063edfd7f42690257e781a91cf8813297fcfc5d7
umathileepan <thileepan_uma@yahoo.co.uk>
2024-03-01 13:31:12 +0000

    Choose different Operations.

---
+++ LearningJava/JuneYoutubeTutorial/lambdacode/Operations.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Operations {
+
+        public static void main(String[] args) {
+            try (Scanner scanner = new Scanner(System.in)) {
+                while (true) {
+                    System.out.println("");
+                    System.out.println("Perform the arithmetic operations by choosing the number");
+                    System.out.println("Enter the first number: ");
+                    int num1 = scanner.nextInt();
+                    System.out.println("Enter the second number: ");
+                    int num2 = scanner.nextInt();
+                    System.out.println("Choose the number to do the calculation: ");
+                    System.out.println("Choose 1 for Addition");
+                    System.out.println("Choose 2 for Subtraction");
+                    System.out.println("Choose 3 for Multiplication");
+                    System.out.println("Choose 4 for Division");
+                    System.out.println("Choose 5 for Module operation");
+                    System.out.println("Choose 6 to exit");
+                    int n = scanner.nextInt();
+                    switch (n){
+                        case 1:
+                            int add = num1 + num2;
+                            System.out.println(add);
+                            break;
+                        case 2:
+                            int sub = num1 - num2;
+                            System.out.println(sub);
+                            break;
+                        case 3:
+                            int multi = num1 * num2;
+                            System.out.println(multi);
+                            break;
+                        case 4:
+                            int div = num1 / num2;
+                            System.out.println(div);
+                            break;
+                        case 5:
+                            int mod = num1  % num2;
+                            System.out.println(mod);
+                            break;
+                        case 6:
+                            System.exit(0);
+
+                    }
+                }
+            }
+
+        }
+
+    }
