9f25cd1096b468e28f546c4f821a1241f5781bef
umathileepan <thileepan_uma@yahoo.co.uk>
2024-04-18 19:12:50 +0100

    Added service class for Loose coupling

--- LearningJava/April24/firstjobapp/src/main/java/com/umathileepan/firstjobapp/job/JobController.java
+++ LearningJava/April24/firstjobapp/src/main/java/com/umathileepan/firstjobapp/job/JobController.java
@@ -1,9 +1,6 @@
 package com.umathileepan.firstjobapp.job;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,5 +27,11 @@ public class JobController {
         return "Job added successfully";
     }
 
-
+    @GetMapping("/jobs/{id}")
+    public Job getJobById(@PathVariable Long id){
+        Job job = jobService.getJobById(id);
+        if(job != null)
+        return job;
+        return new Job(1L,"TestJob","Test","20000","20000","Loc");
+    }
 }
--- LearningJava/April24/firstjobapp/src/main/java/com/umathileepan/firstjobapp/job/impl/JobServiceImpl.java
+++ LearningJava/April24/firstjobapp/src/main/java/com/umathileepan/firstjobapp/job/impl/JobServiceImpl.java
@@ -21,4 +21,14 @@ public class JobServiceImpl implements JobService {
             job.setId(nextId++);
             jobs.add(job);
     }
+
+    @Override
+    public Job getJobById(Long id) {
+        for(Job job:jobs){
+            if(job.getId().equals(id)){
+                return job;
+            }
+        }
+        return null;
+    }
 }
